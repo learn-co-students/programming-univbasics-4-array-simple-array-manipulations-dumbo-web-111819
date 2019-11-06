f65d232c1b885c3e55f491eae66261f8f3eafcec
Isaac Avilez <isaac1180@aol.com>
2019-11-06 17:42:44 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,59 @@
+def using_push(array, element)    
+  return array.push(element)
+end
+
+def using_unshift(array, string) 
+ return array.unshift(string)
+end
+
+def using_pop(array)  
+  return array.pop()
+end
+
+def pop_with_args(array)
+   array.pop(2)
+end
+
+def using_shift(array)
+  return array.shift
+end
+
+def shift_with_args(array)
+   array.shift(2)
+end
+
+def using_concat(array, array2)
+  array.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  return array.uniq()
+end
+
+def using_flatten(array)
+  return array.flatten()
+end
+
+def using_delete(array, string)
+  return array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  return array.delete_at(integer)
+end
+
+
+
+
+
+
+
+
+
+
+
+
