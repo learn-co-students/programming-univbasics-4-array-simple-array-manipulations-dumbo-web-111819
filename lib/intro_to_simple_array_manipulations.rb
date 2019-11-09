c5b3063599fc2d94f874b762c6e2d42be4a7378f
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-09 02:05:35 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,7 +7,7 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  array.pop()
+  array.pop
 end
 
 def pop_with_args(array)
@@ -15,12 +15,12 @@ def pop_with_args(array)
 end
 
 def using_shift(array)
-  array.shift()
+  array.shift
 end
 
 def shift_with_args(array)
   array.shift(2)
-end
+end 
 
 def using_concat(array1, array2)
   array1.concat(array2)
@@ -29,13 +29,13 @@ end
 def using_insert(array, element)
   array.insert(4, element)
 end
-
+  
 def using_uniq(array)
-  array.uniq()
+  array.uniq 
 end
 
 def using_flatten(array)
-  array.flatten()
+  array.flatten
 end
 
 def using_delete(array, string)
@@ -45,10 +45,16 @@ end
 def using_delete_at(array, integer)
   array.delete_at(integer)
 end
-
-
-
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 
 
